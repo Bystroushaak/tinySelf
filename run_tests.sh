85024ef8c632dc3b319392df3800beb150aeb8f1
Bystroushaak <bystrousak@kitakitsune.org>
2017-04-10 01:25:32 +0200

    Added env variables.

--- run_tests.sh
+++ run_tests.sh
@@ -1,5 +1,6 @@
 #! /usr/bin/env sh
-export PYTHONPATH="src:$PYTHONPATH:/home/bystrousak/Plocha/tests/pypy/rpython"
+export PYTHONPATH="src:$PYTHONPATH:/home/bystrousak/Plocha/tests/pypy/"
+export PYTHONWARNINGS="ignore"
 
 python -m pytest tests $@
 # python3 -m pytest tests $@
