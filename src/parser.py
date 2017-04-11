09bf780586a381c57edf285701c058e7e0e37df6
Bystroushaak <bystrousak@kitakitsune.org>
2017-04-11 23:02:49 +0200

    Added rules to parse object and message sends.

--- src/parser.py
+++ src/parser.py
@@ -10,9 +10,15 @@ from rpython.rlib.parsing.ebnfparse import make_parse_function
 regexs, rules, transformer = parse_ebnf("""
 IGNORE: " ";
 
-expressions: IDENTIFIER | value | object;
+object: ["("] slots? expressions* [")"];
 
-object: ["("] (slots)* (expressions)* [")"];
+expressions: (<send> ["."])* <send> ["."]?;
+stuff: IDENTIFIER | value | object;
+
+send: receiver? keyword | receiver? message;
+receiver: IDENTIFIER;
+message: IDENTIFIER;
+keyword: FIRST_KW_IDENTIFIER >stuff< (KEYWORD_IDENTIFIER >stuff<)*;
 
 slots: ["|"] (IDENTIFIER ["."])* ["|"];
 
@@ -20,30 +26,45 @@ value: <string> | <float> | <integer>;
 
 comment: "\#.*\n";
 
-IDENTIFIER: "[a-zA-Z_][a-zA-Z0-9_]*";
+float: integer "\." POSINT;
+
+integer: "\-" POSINT | POSINT;
+
+POSINT: "0|[1-9][0-9]*";
+
+IDENTIFIER: "[a-zA-Z_][a-zA-Z0-9_\*]*";
+FIRST_KW_IDENTIFIER: "[a-z][a-zA-Z0-9_]*:";
+KEYWORD_IDENTIFIER: "[A-Z][a-zA-Z0-9_]*:";
 
 string: SINGLE_QUOTED_STRING | DOUBLE_QUOTED_STRING;
 SINGLE_QUOTED_STRING: "'[^\\\']*'";
 DOUBLE_QUOTED_STRING: "\\"[^\\\\"]*\\"";
-
-float: integer "\." integer;
-integer: "\-" POSINT | POSINT;
-POSINT: "0|[1-9][0-9]*";
 """)
+# expressions: (stuff ".") | <expressions>*;
 
 
 parse = make_parse_function(regexs, rules)
 # parse = make_parse_function(regexs, rules, eof=True)
 
-print transformer().transform(parse("0"))
-print transformer().transform(parse("11"))
-print transformer().transform(parse("-23"))
-print transformer().transform(parse("1.35"))
-print transformer().transform(parse('"a"'))
-print transformer().transform(parse("'č'"))
-print transformer().transform(parse("identifier"))
-print transformer().transform(parse("IDENTIFIER"))
-print transformer().transform(parse("_identifier"))
-print transformer().transform(parse("_Identifier"))
+# print transformer().transform(parse("0"))
+# print transformer().transform(parse("11"))
+# print transformer().transform(parse("-23"))
+# print transformer().transform(parse("1.35"))
+# print transformer().transform(parse('"a"'))
+# print transformer().transform(parse("'č'"))
+# print transformer().transform(parse("identifier"))
+# print transformer().transform(parse("IDENTIFIER"))
+# print transformer().transform(parse("_identifier"))
+# print transformer().transform(parse("_Identifier"))
 print transformer().transform(parse("(| asd. bsd. | asd)"))
+print
+print transformer().transform(parse("(| asd. bsd. | asd. bsd. obj msg.)"))
+print
+print transformer().transform(parse("(| asd. bsd. | asd. bsd. obj msg.)"))
+print
+print transformer().transform(parse("(| asd. bsd. | self hello: xe.)"))
+print
+print transformer().transform(parse("(| asd. bsd. | self hello: xe Second: xa.)"))
+print
+print transformer().transform(parse("(| asd. bsd. | hello: xe.)"))
 # print parse("0 + 10 + 999")
