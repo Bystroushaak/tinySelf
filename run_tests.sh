6406034038b7b996edcbad7ea459bfc72126197a
Bystroushaak <bystrousak@kitakitsune.org>
2017-12-10 15:48:55 +0100

    Everything was thrown out.

--- run_tests.sh
+++
@@ -1,6 +0,0 @@
-#! /usr/bin/env sh
-export PYTHONPATH="src:$PYTHONPATH:/home/bystrousak/Plocha/tests/pypy/"
-export PYTHONWARNINGS="ignore"
-
-python -m pytest tests $@
-# python3 -m pytest tests $@
