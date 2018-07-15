0ca6eb0555ac272b932cc951580510c2cb5c926d
Bystroushaak <bystrousak@kitakitsune.org>
2018-07-15 15:46:05 +0200

    Added new switch -c / --compile to test compilation process.

--- src/target.py
+++ src/target.py
@@ -63,6 +63,22 @@ def run_script(path):
     return 0
 
 
+def compile_file(path):
+    print "Compiling script", path  # TODO: remove
+
+    with open(path) as f:
+        try:
+            for expr in lex_and_parse(f.read()):
+                print expr.__str__()
+        except ParsingError as e:
+            ewriteln("Parse error.")
+            if e.message:
+                ewriteln(e.message)
+            return 1
+
+    return 0
+
+
 # def run_snapshot(path):
 #     print "Running snapshot", path
 #     return 0
@@ -70,13 +86,16 @@ def run_script(path):
 
 def print_help(fn):
     ewriteln("Usage:")
-    ewriteln("\t%s [-h, -v] [-f] FN / SCRIPT_PATH" % fn)
+    ewriteln("\t%s [-h, -v] [-f FN] [-c FN] [-a FN]" % fn)
     ewriteln("")
     ewriteln("\t-a FN, --ast FN")
     ewriteln("\t\tShow AST of the `FN`.")
     ewriteln("")
     ewriteln("\t-f FN, --filename FN")
     ewriteln("\t\tRun `FN` as a tinySelf script.")
+    ewriteln("")
+    ewriteln("\t-c FN, --compile FN")
+    ewriteln("\t\tCompile FN, output bytecode to the stdout.")
     ewriteln("")
     # ewriteln("\t-s FN, --snapshot FN")
     # ewriteln("\t\tRun memory snapshot `FN`.")
@@ -92,7 +111,7 @@ def print_help(fn):
     ewriteln("")
 
 
-def run_command(command, file):
+def parse_arg_with_file_param(command, file):
     if not os.path.exists(file):
         ewriteln("`%s` not found!\n" % file)
         return 1
@@ -109,6 +128,8 @@ def run_command(command, file):
             return 0
     elif command in ["-f", "--filename"]:
         return run_script(file)
+    elif command in ["-c", "--compile"]:
+        return compile_file(file)
     # elif command in ["-s", "--snapshot"]:
     #     return run_script(file)
 
@@ -124,18 +145,27 @@ def parse_args(argv):
         if argv[1] in ["-h", "--help"]:
             print_help(argv[0])
             return 0
+
         if argv[1] in ["-v", "--version"]:
             print "tSelf", VERSION
             print get_compiler_info()
             return 0
+
+        elif argv[1].startswith("-"):
+            ewriteln(
+                "%s probably requires a parameter. See --help for details!" % argv[1]
+            )
+            return 1
+
         elif not os.path.exists(argv[1]):
             ewriteln("Unrecognized option `%s`!" % argv[1])
             return 1
+
         else:
             return run_script(argv[1])
 
     elif len(argv) == 3:
-        return run_command(argv[1], argv[2])
+        return parse_arg_with_file_param(argv[1], argv[2])
 
     else:
         ewriteln("Unknown arguments `%s`!" % str(argv[1:]))
@@ -162,3 +192,7 @@ def untranslated_main():
     """
     import sys
     sys.exit(main(sys.argv))
+
+
+if __name__ == '__main__':
+    untranslated_main()
