2d2fff502676c7eba24a2d202e0ef45d3f21025c
Bystroushaak <bystrousak@kitakitsune.org>
2018-09-17 22:35:47 +0200

    Remove old __pycache__ directories when running test.

--- run_tests.sh
+++ run_tests.sh
@@ -1,4 +1,12 @@
 #! /usr/bin/env bash
 export PYTHONPATH="src/:$PYTHONPATH"
 
+if [ -e "tests/__pycache__" ]; then
+    rm -fr "tests/__pycache__"
+fi
+
+if [ -e "tests/vm/__pycache__" ]; then
+    rm -fr "tests/vm/__pycache__"
+fi
+
 python2 -m py.test tests $@
