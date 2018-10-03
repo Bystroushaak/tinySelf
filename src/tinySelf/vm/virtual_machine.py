b180b400facc7fbbd0f92f02add3f91af2aa0e0f
Bystroushaak <bystrousak@kitakitsune.org>
2018-10-03 23:53:07 +0200

    virtual_machine.py filled with boilerplate connection code. #27

--- src/tinySelf/vm/virtual_machine.py
+++ src/tinySelf/vm/virtual_machine.py
@@ -3,16 +3,18 @@ from rply import ParsingError
 
 from tinySelf.parser import lex_and_parse
 from tinySelf.vm.primitives import get_primitives
+from tinySelf.vm.interpreter import Interpreter
 from tinySelf.vm.code_context import CodeContext
 from tinySelf.vm.object_layout import Object
-from tinySelf.vm.primitives import get_primitives
-
-
-
 
 
 def virtual_machine(source):
-    globals_obj = Object()
-    globals_obj.set_slot("primitives", get_primitives())
+    universe = Object()
+    universe.set_slot("primitives", get_primitives())
 
     ast = lex_and_parse(source)
+
+    interpreter = Interpreter(universe)
+    for item in ast:
+        context = item.compile(CodeContext())
+        print interpreter.interpret(context)
