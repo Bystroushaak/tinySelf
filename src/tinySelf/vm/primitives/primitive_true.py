d097c43334f743b4fd3e0b097430e37c07521e56
Bystroushaak <bystrousak@kitakitsune.org>
2018-10-11 00:05:59 +0200

    Added True.is_true() and tests. #48

--- src/tinySelf/vm/primitives/primitive_true.py
+++ src/tinySelf/vm/primitives/primitive_true.py
@@ -1,10 +1,23 @@
 # -*- coding: utf-8 -*-
 from tinySelf.vm.object_layout import Object
+from tinySelf.vm.primitives.primitive_false import PrimitiveFalseObject
+from tinySelf.vm.primitives.add_primitive_fn import add_primitive_fn
+
+
+def is_true(self, parameters):
+    obj = parameters[0]
+
+    if isinstance(obj, PrimitiveTrueObjectSingleton):
+        return PrimitiveTrueObject()
+    else:
+        return PrimitiveFalseObject()
 
 
 class PrimitiveTrueObjectSingleton(Object):
     def __init__(self):
         Object.__init__(self)
+
+        add_primitive_fn(self, "is", is_true, ["obj"])
 
     def clone(self):
         return self
---
+++ tests/vm/primitives/test_primitive_true.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+import pytest
+
+from tinySelf.vm.primitives import PrimitiveIntObject
+from tinySelf.vm.primitives import PrimitiveTrueObject
+from tinySelf.vm.primitives import PrimitiveFalseObject
+
+
+def test_PrimitiveTrueObject():
+    assert PrimitiveTrueObject()
+
+
+def test_PrimitiveTrueObject_is():
+    o = PrimitiveTrueObject()
+
+    is_slot = o.slot_lookup("is")
+    result = is_slot.map.primitive_code(o, [PrimitiveTrueObject()])
+    assert result == PrimitiveTrueObject()
+
+    is_slot = o.slot_lookup("is")
+    result = is_slot.map.primitive_code(o, [PrimitiveIntObject(3)])
+    assert result == PrimitiveFalseObject()
+
+    is_slot = o.slot_lookup("is")
+    result = is_slot.map.primitive_code(o, [PrimitiveFalseObject()])
+    assert result == PrimitiveFalseObject()
