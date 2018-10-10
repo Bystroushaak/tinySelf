c76e905193e06253e0b5f26750602fe5b44b3ccb
Bystroushaak <bystrousak@kitakitsune.org>
2018-10-10 23:57:38 +0200

    Added PrimitiveNilObject.is_nil and tests.

--- src/tinySelf/vm/primitives/primitive_nil.py
+++ src/tinySelf/vm/primitives/primitive_nil.py
@@ -1,10 +1,25 @@
 # -*- coding: utf-8 -*-
 from tinySelf.vm.object_layout import Object
+from tinySelf.vm.primitives.add_primitive_fn import add_primitive_fn
+from tinySelf.vm.primitives.primitive_true import PrimitiveTrueObject
+from tinySelf.vm.primitives.primitive_false import PrimitiveFalseObject
+
+
+def is_nil(self, parameters):
+    obj = parameters[0]
+
+    if isinstance(obj, PrimitiveNilObjectSingleton):
+        return PrimitiveTrueObject()
+    else:
+        return PrimitiveFalseObject()
 
 
 class PrimitiveNilObjectSingleton(Object):
     def __init__(self):
         Object.__init__(self)
+
+        add_primitive_fn(self, "==", is_nil, ["obj"])
+        add_primitive_fn(self, "is", is_nil, ["obj"])
 
     def clone(self):
         return self
---
+++ tests/vm/primitives/test_primitive_nil.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+import pytest
+
+from tinySelf.vm.primitives import PrimitiveIntObject
+from tinySelf.vm.primitives import PrimitiveNilObject
+from tinySelf.vm.primitives import PrimitiveTrueObject
+from tinySelf.vm.primitives import PrimitiveFalseObject
+
+
+def test_PrimitiveNilObject():
+    assert PrimitiveNilObject()
+
+
+def test_PrimitiveNilObject_is():
+    o = PrimitiveNilObject()
+
+    is_slot = o.slot_lookup("is")
+    result = is_slot.map.primitive_code(o, [PrimitiveNilObject()])
+    assert result == PrimitiveTrueObject()
+
+    is_slot = o.slot_lookup("is")
+    result = is_slot.map.primitive_code(o, [PrimitiveIntObject(3)])
+    assert result == PrimitiveFalseObject()
+
+    is_slot = o.slot_lookup("is")
+    result = is_slot.map.primitive_code(o, [PrimitiveTrueObject()])
+    assert result == PrimitiveFalseObject()
+
+
+def test_PrimitiveNilObject_equals():
+    o = PrimitiveNilObject()
+
+    eq_slot = o.slot_lookup("==")
+    result = eq_slot.map.primitive_code(o, [PrimitiveNilObject()])
+    assert result == PrimitiveTrueObject()
+
+    eq_slot = o.slot_lookup("==")
+    result = eq_slot.map.primitive_code(o, [PrimitiveIntObject(3)])
+    assert result == PrimitiveFalseObject()
+
+    eq_slot = o.slot_lookup("==")
+    result = eq_slot.map.primitive_code(o, [PrimitiveTrueObject()])
+    assert result == PrimitiveFalseObject()
