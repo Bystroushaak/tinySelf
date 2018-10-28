3db7031f937953a2f5eeb5b474ddee75f19a48c2
Bystroushaak <bystrousak@kitakitsune.org>
2018-10-28 02:25:28 +0100

    Added test of the mirror. #15

---
+++ tests/vm/primitives/test_mirror.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from tinySelf.vm.primitives import Mirror
+from tinySelf.vm.primitives import PrimitiveIntObject
+from tinySelf.vm.primitives import PrimitiveStrObject
+
+from tinySelf.vm.object_layout import Object
+
+
+def test_mirror():
+    o = Object()
+    m = Mirror(o)
+
+    assert not o.slot_lookup("v")
+
+    add_primitive = m.slot_lookup("toSlot:Add:")
+    assert add_primitive.map.primitive_code
+    result = add_primitive.map.primitive_code(
+        None,
+        m,
+        [PrimitiveStrObject("v"), PrimitiveIntObject(1)]
+    )
+    assert result == o
+
+    assert o.slot_lookup("v") == PrimitiveIntObject(1)
