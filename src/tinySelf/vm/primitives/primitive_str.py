3d6b203dc244413e067c83f92ad1a8b05d66182e
Bystroushaak <bystrousak@kitakitsune.org>
2018-11-09 23:49:29 +0100

    Added PrimitiveStr : print method. #64

--- src/tinySelf/vm/primitives/primitive_str.py
+++ src/tinySelf/vm/primitives/primitive_str.py
@@ -1,5 +1,9 @@
 # -*- coding: utf-8 -*-
+from tinySelf.r_io import write
+from tinySelf.r_io import writeln
+
 from tinySelf.vm.object_layout import Object
+from tinySelf.vm.primitives.primitive_nil import PrimitiveNilObject
 from tinySelf.vm.primitives.add_primitive_fn import add_primitive_fn
 
 
@@ -13,6 +17,15 @@ def add_strings(_, self, parameters):
     return PrimitiveStrObject(self.value + obj.value)
 
 
+def print_string(_, self, parameters):
+    assert isinstance(self, PrimitiveStrObject)
+    assert isinstance(self.value, str)
+
+    write(self.value)
+
+    return PrimitiveNilObject()
+
+
 class PrimitiveStrObject(Object):
     def __init__(self, value, obj_map=None):
         Object.__init__(self, obj_map)
@@ -21,6 +34,7 @@ class PrimitiveStrObject(Object):
         self.value = value
 
         add_primitive_fn(self, "+", add_strings, ["obj"])
+        add_primitive_fn(self, "print", print_string, [])
 
     def __str__(self):
         return "PrimitiveStrObject(%s)" % self.value
