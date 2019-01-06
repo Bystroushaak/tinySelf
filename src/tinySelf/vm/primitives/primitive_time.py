8309d4c23fab913a499290a630b7c591bff52019
Bystroushaak <bystrousak@kitakitsune.org>
2019-01-06 02:38:38 +0100

    Added new primitive; primitive time. Closed #73.

---
+++ src/tinySelf/vm/primitives/primitive_time.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+import time
+
+from tinySelf.vm.object_layout import Object
+from tinySelf.vm.primitives.add_primitive_fn import add_primitive_fn
+from tinySelf.vm.primitives.primitive_float import PrimitiveFloatObject
+
+
+def get_timestamp(context, time_obj, parameters):
+    return PrimitiveFloatObject(time.time())
+
+
+def get_primitive_time_object():
+    time_obj = Object()
+    add_primitive_fn(time_obj, "timestamp", get_timestamp, [])
+
+    return time_obj
