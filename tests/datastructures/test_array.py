c3f46460dd2e9c53b2370cd4b226ed186a71fd6c
Bystroushaak <bystrousak@kitakitsune.org>
2019-04-10 21:45:16 +0200

    Added arrays which are slightly faster.

---
+++ tests/datastructures/test_array.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+from tinySelf.datastructures.arrays import TwoPointerArray
+
+
+def test_twopointerarray():
+    tpa = TwoPointerArray(5)
+    assert len(tpa) == 0
+
+    tpa.append(1)
+    assert len(tpa) == 1
+    assert tpa[0] == 1
+
+    tpa.append(2)
+    tpa.append(3)
+    tpa.append(4)
+    tpa.append(5)
+    tpa.append(6)
+
+    assert len(tpa) == 6
+    assert tpa[0] == 1
+    assert tpa[1] == 2
+    assert tpa[2] == 3
+    assert tpa[3] == 4
+    assert tpa[4] == 5
+    assert tpa[5] == 6
+
+    assert tpa.pop_last() == 6
+    assert len(tpa) == 5
+    assert tpa[0] == 1
+    assert tpa[1] == 2
+    assert tpa[2] == 3
+    assert tpa[3] == 4
+    assert tpa[4] == 5
+
+    assert tpa.pop_first() == 1
+    assert len(tpa) == 4
+    assert tpa[0] == 2
+    assert tpa[1] == 3
+    assert tpa[2] == 4
+    assert tpa[3] == 5
+
+    assert tpa.to_list() == [2, 3, 4, 5]
