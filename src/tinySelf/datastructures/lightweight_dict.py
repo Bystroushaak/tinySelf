6b34b3fc7b1e9f5953ff9e5c7a688f0e77cc7ab1
Bystroushaak <bystrousak@kitakitsune.org>
2019-04-17 00:03:51 +0200

    Experimental version with 2 properties.

--- src/tinySelf/datastructures/lightweight_dict.py
+++ src/tinySelf/datastructures/lightweight_dict.py
@@ -630,8 +630,6 @@ class LightWeightDictJustProperties(object):
         self._first_value = None
         self._second_key = None
         self._second_value = None
-        self._third_key = None
-        self._third_value = None
 
         self._use_properties = True
         self._use_dict = False
@@ -641,8 +639,7 @@ class LightWeightDictJustProperties(object):
     def has_key(self, key):
         if self._use_properties:
             return key == self._first_key or \
-                   key == self._second_key or \
-                   key == self._third_key
+                   key == self._second_key
         else:
             return key in self._dict
 
@@ -657,9 +654,6 @@ class LightWeightDictJustProperties(object):
             elif self._second_key is None or self._second_key == key:
                 self._second_key = key
                 self._second_value = Container(val)
-            elif self._third_key is None or self._third_key == key:
-                self._third_key = key
-                self._third_value = Container(val)
             else:
                 self._use_properties = False
                 self._use_dict = True
@@ -667,14 +661,11 @@ class LightWeightDictJustProperties(object):
 
                 self._dict[self._first_key] = self._first_value.val
                 self._dict[self._second_key] = self._second_value.val
-                self._dict[self._third_key] = self._third_value.val
 
                 self._first_key = None
                 self._first_value = None
                 self._second_key = None
                 self._second_value = None
-                self._third_key = None
-                self._third_value = None
 
                 return self.set(key, val)
 
@@ -690,8 +681,6 @@ class LightWeightDictJustProperties(object):
                 return self._first_value.val
             elif self._second_key == key:
                 return self._second_value.val
-            elif self._third_key == key:
-                return self._third_value.val
             else:
                 return alt
         else:
@@ -703,8 +692,6 @@ class LightWeightDictJustProperties(object):
                 return self._first_value.val
             elif self._second_key == key:
                 return self._second_value.val
-            elif self._third_key == key:
-                return self._third_value.val
             else:
                 raise KeyError("`%s` not found." % key)
         else:
@@ -714,23 +701,14 @@ class LightWeightDictJustProperties(object):
         if self._use_properties:
             if self._first_key == key:
                 self._first_key = self._second_key
-                self._second_key = self._third_key
-                self._third_key = None
+                self._second_key = None
 
                 self._first_value = self._second_value
-                self._second_value = self._third_value
-                self._third_value = None
+                self._second_value = None
 
             elif self._second_key == key:
-                self._second_key = self._third_key
-                self._third_key = None
-
-                self._second_value = self._third_value
-                self._third_value = None
-
-            elif self._third_key == key:
-                self._third_key = None
-                self._third_value = None
+                self._second_key = None
+                self._second_value = None
 
         else:
             if key in self._dict:
@@ -746,8 +724,6 @@ class LightWeightDictJustProperties(object):
                 keys.append(self._first_key)
             if self._second_key is not None:
                 keys.append(self._second_key)
-            if self._third_key is not None:
-                keys.append(self._third_key)
 
             return keys
 
@@ -761,8 +737,6 @@ class LightWeightDictJustProperties(object):
                 values.append(self._first_value.val)
             if self._second_key is not None:
                 values.append(self._second_value.val)
-            if self._third_key is not None:
-                values.append(self._third_value.val)
 
             return values
 
@@ -775,8 +749,6 @@ class LightWeightDictJustProperties(object):
             if self._first_key is not None:
                 length += 1
             if self._second_key is not None:
-                length += 1
-            if self._third_key is not None:
                 length += 1
 
             return length
@@ -792,8 +764,6 @@ class LightWeightDictJustProperties(object):
             lwd._first_value = self._first_value
             lwd._second_key = self._second_key
             lwd._second_value = self._second_value
-            lwd._third_key = self._third_key
-            lwd._third_value = self._third_value
         else:
             lwd._dict = self._dict
 
@@ -808,9 +778,7 @@ class LightWeightDictJustProperties(object):
                 return (self._first_key == other._first_key and
                         self._first_value == other._first_value and
                         self._second_key == other._second_key and
-                        self._second_value == other._second_value and
-                        self._third_key == other._third_key and
-                        self._third_value == other._third_value)
+                        self._second_value == other._second_value)
 
             elif self._use_dict and other._use_dict:
                 return self._dict == other._dict
@@ -847,8 +815,6 @@ class LightWeightDictJustProperties(object):
                 yield self._first_key, self._first_value.val
             if self._second_key is not None:
                 yield self._second_key, self._second_value.val
-            if self._third_key is not None:
-                yield self._third_key, self._third_value.val
 
         else:
             for k, v in self._dict.iteritems():
