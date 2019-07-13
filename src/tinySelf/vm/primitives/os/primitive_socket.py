5ee154cb284aeee9d9d9754f9c755f14d07200d6
Bystroushaak <bystrousak@kitakitsune.org>
2019-07-13 03:44:11 +0200

    Added most basic socket wrapper. #46

--- src/tinySelf/vm/primitives/os/primitive_socket.py
+++ src/tinySelf/vm/primitives/os/primitive_socket.py
@@ -1,10 +1,14 @@
 # -*- coding: utf-8 -*-
-from rpython.rlib import rsocket
+from rpython.rlib.rsocket import AF_INET
+from rpython.rlib.rsocket import SOCK_STREAM
+from rpython.rlib.rsocket import RSocket
+from rpython.rlib.rsocket import INETAddress
 
 from tinySelf.vm.object_layout import Object
 
 from tinySelf.vm.primitives.cache import ObjCache
 from tinySelf.vm.primitives.primitive_int import PrimitiveIntObject
+from tinySelf.vm.primitives.primitive_int import PrimitiveStrObject
 from tinySelf.vm.primitives.add_primitive_fn import add_primitive_fn
 
 
@@ -13,20 +17,38 @@ class PrimitiveINETAddress(Object):
         pass
 
 
+def socket_recv(interpreter, pseudo_self, parameters):
+    size_obj = parameters[0]
+    assert isinstance(size_obj, PrimitiveIntObject)
+    assert isinstance(pseudo_self, PrimitiveSocketObject)
+
+    return PrimitiveStrObject(pseudo_self.value.recv(size_obj.value))
+
+
+def socket_sendall(interpreter, pseudo_self, parameters):
+    content_obj = parameters[0]
+    assert isinstance(content_obj, PrimitiveStrObject)
+    assert isinstance(pseudo_self, PrimitiveSocketObject)
+
+    pseudo_self.value.sendall(content_obj.value)
+    return pseudo_self
+
+
 class PrimitiveSocketObject(Object):
     _OBJ_CACHE = ObjCache()
     _immutable_fields_ = ["value"]
     def __init__(self, value, obj_map=None):
         Object.__init__(self, PrimitiveSocketObject._OBJ_CACHE.map)
 
-        assert isinstance(value, list)
+        assert isinstance(value, RSocket)
         self.value = value
 
         if PrimitiveSocketObject._OBJ_CACHE.map is not None:
             self._slot_values = PrimitiveSocketObject._OBJ_CACHE.slots
             return
 
-        # add_primitive_fn(self, "at:", list_at, ["index"])
+        add_primitive_fn(self, "recv:", socket_recv, ["size"])
+        add_primitive_fn(self, "sendAll:", socket_sendall, ["data"])
 
         if PrimitiveSocketObject._OBJ_CACHE.map is None:
             PrimitiveSocketObject._OBJ_CACHE.store(self)
@@ -41,9 +63,23 @@ class PrimitiveSocketObject(Object):
         return self.value == obj.value
 
 
+def open_socket(interpreter, pseudo_self, parameters):
+    host_obj = parameters[0]
+    assert isinstance(host_obj, PrimitiveStrObject)
+    port_obj = parameters[1]
+    assert isinstance(port_obj, PrimitiveIntObject)
+
+    # TODO: also add for IPv6 and unix
+    addr = INETAddress(host_obj.value, port_obj.value)
+    socket = RSocket(AF_INET, SOCK_STREAM)
+    socket.connect(addr)
+
+    return PrimitiveSocketObject(socket)
+
+
 def get_primitive_socket():
     socket_obj = Object()
 
-    # add_primitive_fn(socket_obj, "open:", open_socket, [""])
+    add_primitive_fn(socket_obj, "open:Port:", open_socket, ["addr", "port"])
 
     return socket_obj
