f3403194168df887937792ebea5c69ea2aac604f
Bystroushaak <bystrousak@kitakitsune.org>
2019-08-04 23:44:50 +0200

    `==` removed from the PrimitiveList, added `clear`. Fixed repr output. #117

--- src/tinySelf/vm/primitives/primitive_list.py
+++ src/tinySelf/vm/primitives/primitive_list.py
@@ -11,7 +11,7 @@ from tinySelf.vm.primitives.add_primitive_fn import add_primitive_fn
 def list_clone(interpreter, self, parameters):
     assert isinstance(self, PrimitiveListObject)
 
-    return PrimitiveListObject(self.value[:])
+    return self._copy_with_primitives(PrimitiveListObject(self.value[:]))
 
 
 def list_append(interpreter, self, parameters):
@@ -67,24 +67,6 @@ def list_reversed(interpreter, self, parameters):
     return PrimitiveListObject([x for x in reversed(self.value)])
 
 
-def list_eq(interpreter, pseudo_self, parameters):
-    assert isinstance(pseudo_self, PrimitiveListObject)
-    other = parameters[0]
-
-    if not isinstance(other, PrimitiveListObject):
-        return PrimitiveFalseObject()
-
-    if len(pseudo_self.value) != len(other.value):
-        return PrimitiveFalseObject()
-
-    for i in range(len(pseudo_self.value)):
-        # TODO: eval inside tinySelf
-        if pseudo_self.value[i] != other.value[i]:
-            return PrimitiveFalseObject()
-
-    return PrimitiveTrueObject()
-
-
 def list_clear(interpreter, pseudo_self, parameters):
     assert isinstance(pseudo_self, PrimitiveListObject)
 
@@ -114,18 +96,19 @@ class PrimitiveListObject(Object):
         add_primitive_fn(self, "at:Put:", list_at_i_put_x, ["index", "obj"])
         add_primitive_fn(self, "reversed", list_reversed, [])
         add_primitive_fn(self, "clear", list_clear, [])
-        add_primitive_fn(self, "==", list_eq, ["obj"])
 
-        if PrimitiveListObject._OBJ_CACHE.map is None:
-            PrimitiveListObject._OBJ_CACHE.store(self)
+        PrimitiveListObject._OBJ_CACHE.store(self)
 
     def __str__(self):
         # I would use something more elegant, but rpython doesn't let me..
         items = ""
-        for x in self.value:
+        for cnt, x in enumerate(self.value):
             items += x.__str__()
 
-        return "[%s]" % items
+            if cnt < len(self.value) - 1:
+                items += ", "
+
+        return "[%s] asList" % items
 
     def __eq__(self, obj):
         if not isinstance(obj, PrimitiveListObject):
