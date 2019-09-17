104cf1fcf18970ddc027c0fb47f0dbe42b968981
Bystroushaak <bystrousak@kitakitsune.org>
2019-09-17 22:19:04 +0200

    debug_plantuml_repr() moved to visualisations.py and refactored.

--- src/tinySelf/vm/debug/visualisations.py
+++ src/tinySelf/vm/debug/visualisations.py
@@ -1,97 +1,162 @@
 # -*- coding: utf-8 -*-
 from plantuml_composer import Root
-from plantuml_composer import Object
+from plantuml_composer import Object as PUObject
 from plantuml_composer import Frame
+
+from tinySelf.vm import Object
 
 
 OBJ_MAP_COUNTER = 0
 FRAMESET_COUNTER = 0
 
 
-def dump_obj_to_plantuml(f, o):
-    name = o.__class__.__name__
-    if o and o.is_block:
-        name = "Block"
+class ObjToPlantUML(object):
+    def __init__(self, obj, recursive=False, _processed_ids=None):
+        self.obj = obj
+        self.recursive = recursive
 
-    f.write('class %s as "%s (%s)" {\n' % (id(o), id(o), name))
+        self._plantuml_repr = ""
 
-    if o:
-        f.write("    Params: %s\n" % o.parameters)
-        for key in o.slot_keys:
-            f.write("    %s (%s)\n" % (key, id(o.get_slot(key))))
+        if _processed_ids is None:
+            self._processed_ids = set()
+        else:
+            self._processed_ids = _processed_ids
 
-    f.write("}\n")
+    def _write(self, s):
+        self._plantuml_repr += s
 
-    if o is None:
-        return None, {}
+    def _writeln(self, s):
+        self._write(s)
+        self._write('\n')
 
-    return o.scope_parent, o.expensive_parent_slots
+    def _reset(self):
+        self._plantuml_repr = ""
+        self._processed_ids = set()
+
+    def dump_to_file(self, file_handle):
+        self._reset()
+
+        file_handle.write("@startuml\n")
+
+        plantuml_repr, connections = self._dump_obj_to_plantuml()
+        file_handle.write(plantuml_repr)
+
+        # conneections have to be written after the classes in order to use
+        # customized names in plantuml
+        for connection in connections:
+            file_handle.write(connection)
+
+        file_handle.write("@enduml\n")
+
+    def _dump_obj_to_plantuml(self):
+        """
+        Dump information about object to plantuml.
+
+        Returns:
+            tuple[str, list]: Description of all objects, list of all relations.
+        """
+        if self.obj.id in self._processed_ids:
+            return '', []
+        else:
+            self._processed_ids.add(self.obj.id)
+
+        name = self.obj.__class__.__name__
+        self._writeln('class %s as "%s (id: %s)" {' % (self.obj.id, name, self.obj.id))
+
+        self._dump_parameters()
+        objects_to_process = []
+        objects_to_process.extend(self._dump_parents())
+        objects_to_process.extend(self._dump_slots())
+        self._dump_ast()
+
+        self._writeln('}')
+
+        relations = []
+        for is_parent, obj_name, other_obj in objects_to_process:
+            obj_dumper = ObjToPlantUML(other_obj, self.recursive, self._processed_ids)
+            obj_repr, obj_links = obj_dumper._dump_obj_to_plantuml()
+
+            self._writeln(obj_repr)
+            obj_type = "parent" if is_parent else "slot"
+            line_type = "-->" if is_parent else "..>"
+
+            relations.append('%s %s %s: %s %s\n' % (self.obj.id, line_type, other_obj.id, obj_type, obj_name))
+            relations.extend(obj_links)
+
+        if self.obj.scope_parent:
+            relations.append('%s --> %s: scope_parent\n' % (self.obj.id, self.obj.scope_parent.id))
+
+            obj_dumper = ObjToPlantUML(self.obj.scope_parent, self.recursive, self._processed_ids)
+            scope_parent_repr, links = obj_dumper._dump_obj_to_plantuml()
+            self._writeln(scope_parent_repr)
+            relations.extend(links)
+
+        return self._plantuml_repr, relations
+
+    def _dump_parents(self):
+        if self.obj._parent_slot_values is None:
+            return
+
+        self._writeln('  -- parents: --')
+
+        if self.obj.scope_parent:
+            self._writeln('  scope_parent = id(%s)' % self.obj.scope_parent.id)
+
+        for parent_name, parent in zip(self.obj.map._parent_slots.keys(), self.obj._parent_slot_values):
+            self._writeln('  ""%s*"" = id(%s)' % (parent_name, parent.id))
+            yield [True, parent_name, parent]
+
+    def _dump_slots(self):
+        if self.obj._slot_values is None:
+            return
+
+        self._writeln('  -- slots: --')
+        for slot_name, slot in zip(self.obj.slot_keys, self.obj._slot_values):
+            self._writeln('  ""%s"" = id(%s)' % (slot_name, slot.id))
+
+            if self.recursive:
+                yield [False, slot_name, slot]
+
+    def _dump_ast(self):
+        if self.obj.ast is None or not self.obj.ast.source_pos.source_snippet:
+            return
+
+        self._writeln('  -- source: --')
+
+        # creole pre-formatted text line by line; ugly, but only way I was able to make work
+        for line in self.obj.ast.source_pos.source_snippet.splitlines():
+            self._writeln('  ""%s""' % line)
+
+    def _dump_parameters(self):
+        if not self.obj.parameters:
+            return
+
+        self._writeln('  -- parameters: --')
+        for parameter in self.obj.parameters:
+            self._writeln('  ' + parameter)  # TODO: plantuml escaping
 
 
-def _get_object_graph(obj):
-    objs_to_print = []
-
-    objs = [obj]
-    while objs:
-        o = objs.pop(0)
-
-        if o in objs_to_print:
-            continue
-
-        objs_to_print.append(o)
-        if o is None:
-            continue
-
-        objs.append(o.scope_parent)
-
-        if o._parent_slot_values is not None:
-            objs.extend(o._parent_slot_values)
-
-    return objs_to_print
-
-
-def obj_map_to_plantuml(obj, numbered=False, print_depth=False, prefix="obj_parent_map"):
-    if numbered:
+def obj_map_to_plantuml(obj, number_files=False, recursive=False, prefix="obj_parent_map"):
+    if number_files:
         global OBJ_MAP_COUNTER
-        f = open("%s_%d.plantuml" % (prefix, OBJ_MAP_COUNTER), "w")
+        filename = "%s_%d.plantuml" % (prefix, OBJ_MAP_COUNTER)
         OBJ_MAP_COUNTER += 1
     else:
-        f = open("%s.plantuml" % prefix, "w")
+        filename = "%s.plantuml" % prefix
 
-    f.write("@startuml\n")
-
-    objs_to_print = _get_object_graph(obj)
-
-    connections = []
-    for o in objs_to_print:
-        sp, parents = dump_obj_to_plantuml(f, o)
-
-        connections.append("%s --> %s: scope_parent\n" % (id(o), id(sp)))
-        for key, val in parents.iteritems():
-            connections.append("%s --> %s: parent %s\n" % (id(o), id(val), key))
-
-    # conneections have to be written after the classes in order to use
-    # customized names in plantuml
-    for connection in connections:
-        f.write(connection)
-
-    f.write("@enduml\n")
-    f.close()
-
-    if print_depth:
-        print
-        print "depth", len(objs_to_print)
-        print
+    obj_dumper = ObjToPlantUML(obj, recursive)
+    with open(filename, "w") as f:
+        obj_dumper.dump_to_file(f)
 
 
 def _render_object(obj, name=None):
     if obj is None:
-        return Object("None")
+        return PUObject("None")
 
     if not name:
         name = str(id(obj))
 
-    o = Object(name)
+    o = PUObject(name)
     o.add_property("__class__: " + obj.__class__.__name__)
     o.add_property("scope_parent: %s" % id(obj.scope_parent))
 
@@ -116,7 +181,7 @@ def _render_object(obj, name=None):
 def _render_method_stack(cnt, method_stack):
     f = Frame(name=str(cnt))
 
-    settings = Object("settings%s" % cnt, "settings", prop=[
+    settings = PUObject("settings%s" % cnt, "settings", prop=[
         "bc_index = %d" % method_stack.bc_index,
         "code_context = %s" % method_stack.code_context,
         # "code_context.self = %s" % method_stack.code_context.self,
@@ -145,7 +210,7 @@ def _render_method_stack(cnt, method_stack):
 def process_stack_to_plantuml(process, numbered=False, prefix="frame"):
     ps = Frame(name="process_stack")
 
-    settings = Object("settings")
+    settings = PUObject("settings")
     settings.add_property("result: %s" % process.result)
     settings.add_property("finished: %s" % process.finished)
     settings.add_property("finished_with_error: %s" % process.finished_with_error)
@@ -182,8 +247,8 @@ def process_stack_to_plantuml(process, numbered=False, prefix="frame"):
 
 
 if __name__ == '__main__':
-    from frames import MethodStack
-    from frames import ProcessStack
+    from tinySelf.vm.frames import MethodStack
+    from tinySelf.vm.frames import ProcessStack
 
     p = ProcessStack()
     process_stack_to_plantuml(p)
