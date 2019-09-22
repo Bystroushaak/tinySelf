44343380473beac78df9746590a857af38c2cf71
Bystroushaak <bystrousak@kitakitsune.org>
2019-09-23 01:00:56 +0200

    Added more tests. #129

--- tests/datastructures/test_array.py
+++ tests/datastructures/test_array.py
@@ -1,11 +1,31 @@
 # -*- coding: utf-8 -*-
+from pytest import raises
+from pytest import fixture
+
 from tinySelf.shared.arrays import TwoPointerArray
 
 
-def test_twopointerarray():
-    tpa = TwoPointerArray(5)
-    assert len(tpa) == 0
+@fixture
+def tpa() :
+    return TwoPointerArray(5)
 
+
+@fixture
+def tpa_3() :
+    tpa_obj = TwoPointerArray(10)
+    tpa_obj.append(1)
+    tpa_obj.append(2)
+    tpa_obj.append(3)
+
+    return tpa_obj
+
+
+def test_twopointerarray_len(tpa, tpa_3):
+    assert len(tpa) == 0
+    assert len(tpa_3) == 3
+
+
+def test_twopointerarray_append_pop_last_pop_first(tpa):
     tpa.append(1)
     assert len(tpa) == 1
     assert tpa[0] == 1
@@ -39,4 +59,35 @@ def test_twopointerarray():
     assert tpa[2] == 4
     assert tpa[3] == 5
 
-    assert tpa.to_list() == [2, 3, 4, 5]
+
+def test_tpa_to_list(tpa):
+    tpa.append(1)
+    tpa.append(2)
+    tpa.append(3)
+    tpa.append(4)
+
+    assert tpa.pop_last() == 4
+    assert tpa.pop_first() == 1
+
+    assert tpa.to_list() == [2, 3]
+
+
+def test_tpa_to_list3(tpa_3):
+    assert tpa_3.to_list() == [1, 2, 3]
+
+    assert tpa_3.pop_last() == 3
+    assert tpa_3.pop_first() == 1
+
+    assert tpa_3.to_list() == [2]
+
+
+def test_get(tpa):
+    with raises(IndexError):
+        tpa[0]
+
+    tpa.append(1)
+
+    assert tpa[0] == 1
+
+    with raises(IndexError):
+        tpa[1]
