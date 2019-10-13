14a5d703dc46991e55013c29ed94779bb712c514
Bystroushaak <bystrousak@kitakitsune.org>
2019-10-13 22:37:23 +0200

    ObjCache now stores also parents. Added method .restore().

--- src/tinySelf/vm/primitives/cache.py
+++ src/tinySelf/vm/primitives/cache.py
@@ -13,7 +13,22 @@ class ObjCache(object):
     def __init__(self):
         self.map = None
         self.slots = None
+        self.parents = None
+
+        self.is_set = False
 
     def store(self, obj):
+        """
+        Args:
+            obj (Object):
+        """
         self.map = obj.map
         self.slots = obj._slot_values
+        self.parents = obj._parent_slot_values
+
+        self.is_set = True
+
+    def restore(self, obj):
+        obj.map = self.map
+        obj._slot_values = self.slots
+        obj._parent_slot_values = self.parents
