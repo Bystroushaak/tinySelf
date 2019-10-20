5e82d76f2123ef3fd02f49deeabb52ef80aba66a
Bystroushaak <bystrousak@kitakitsune.org>
2019-10-21 00:27:38 +0200

    LinkedList is faster with JIT, and PreallocatedArray without it.

--- src/tinySelf/config.py
+++ src/tinySelf/config.py
@@ -3,6 +3,7 @@
 VERSION = "0.0.1"
 
 # Use MethodStack based on LinkedList (=True) or PreallocatedArray (=False)?
-USE_LINKED_LIST_METHOD_STACK = False
+# LinkedList is faster with JIT, and PreallocatedArray without it.
+USE_LINKED_LIST_METHOD_STACK = True
 
 STDLIB_PATHS = "objects/stdlib.tself:/var/lib/tinySelf/stdlib.tself"
